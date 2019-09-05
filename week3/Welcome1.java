c8a6646cb5e5c59db82627e0a8637cbc73b323d1
obrienke <kevin.obrien@gmit.ie>
2019-09-05 14:26:18 +0000

    Week 3 updates

--- week3/Welcome1.java
+++ week3/Welcome1.java
@@ -1,5 +1,5 @@
 public class Welcome1{
   public static void main(String[] args){
- 	    System.out.println("Welcome to java programming.");
+ 	    System.out.println("Welcome to java programming again.");
   }
 }
